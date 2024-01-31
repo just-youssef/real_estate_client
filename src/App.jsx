8818d97942ddde6980d832eff26943a5b0016946
Youssef Hussein <61589133+just-youssef@users.noreply.github.com>
2024-01-31 03:58:37 +0200

    remove exact

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,18 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/sign-in" element={<SignIn />} />
-        <Route exact path="/sign-up" element={<SignUp />} />
-        <Route exact path="/verification/:id" element={<Verification />} />
-        <Route exact path="/verification-confirm/:id/:token" element={<VerificationConfirm />} />
-        <Route exact path="/listing/:id" element={<ListingDetails />} />
-        <Route exact path="/search" element={<Search />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/verification/:id" element={<Verification />} />
+        <Route path="/verification-confirm/:id/:token" element={<VerificationConfirm />} />
+        <Route path="/listing/:id" element={<ListingDetails />} />
+        <Route path="/search" element={<Search />} />
         <Route element={<Private />}>
-          <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/listing/create" element={<CreateListing />} />
-          <Route exact path="/listing/update/:id" element={<UpdateListing />} />
-          <Route exact path="/listing/gallery" element={<ListingGallery />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/listing/create" element={<CreateListing />} />
+          <Route path="/listing/update/:id" element={<UpdateListing />} />
+          <Route path="/listing/gallery" element={<ListingGallery />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
